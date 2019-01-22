a0ac7544a67388f5c0c8792fe787a34c0e93fd5c
Sachin Gevariya <s.gevariya@gmail.com>
2019-01-22 18:34:31 +0530

    Updated first designed resume with site

---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+
+(function ($) {
+
+	"use strict";
+
+	$('a[href="#"]').on('click', function(event){
+		return;
+	});
+	
+	
+	if ( $.isFunction($.fn.fluidbox) ) {
+		$('a').fluidbox();
+	}
+	
+	var countCounterUp = 0;
+	
+	var a = 0 ;
+	
+	countCounterUp = enableCounterUp(countCounterUp);
+	
+	$(window).on('scroll', function(){
+		
+		countCounterUp = enableCounterUp(countCounterUp);
+	
+	});
+
+})(jQuery);
+
+
+// Testimonial slider
+$(document).ready(function() {
+  $(".testimonial-carousel").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: true,
+		speed: 500,
+    fade: true,
+    prevArrow: $(".testimonial-carousel-controls .prev"),
+    nextArrow: $(".testimonial-carousel-controls .next")
+  });
+});
+
+
+function enableCounterUp(a){
+	
+	var counterElement;
+	
+	if(isExists('#counter')){ counterElement = $('#counter'); }
+	else{ return; }
+		
+	var oTop = $('#counter').offset().top - window.innerHeight;
+	if (a == 0 && $(window).scrollTop() > oTop) {
+		$('.counter-value').each(function() {
+			var $this = $(this),
+				countDuration = $this.data('duration'),
+				countTo = $this.attr('data-count');
+			$({
+				countNum: $this.text()
+			}).animate({
+				countNum: countTo
+			},{
+
+				duration: countDuration,
+				easing: 'swing',
+				step: function() {
+					$this.text(Math.floor(this.countNum));
+				},
+				complete: function() {
+					$this.text(this.countNum);
+				}
+
+			});
+		});
+		a = 1;
+	}
+
+	return a;
+}
+
+function isExists(elem){
+	if ($(elem).length > 0) { 
+		return true;
+	}
+	return false;
+}
